5fc1ec635e34c2f7efed83985521d865f294611b
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-04-07 19:04:20 -0400

    spinner changes test

--- frontend/components/Spinner.js
+++ frontend/components/Spinner.js
@@ -25,7 +25,7 @@ export default function Spinner({ on }) {
   if (!on) return null
   return (
     <StyledSpinner id="spinner">
-      <h3>&nbsp;.</h3>&nbsp;&nbsp;&nbsp;Please wait...
+      <h3>&nbsp;.</h3>&nbsp;&nbsp;&nbsp;Loading...
     </StyledSpinner>
   )
 }
